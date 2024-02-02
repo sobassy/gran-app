75b486ab91146cc101aa9af744561f48bf326515
Sobassy <altoscience1110@gmail.com>
2024-02-03 02:41:56 +0900

    add: top category page

--- app/page.tsx
+++ app/page.tsx
@@ -1,262 +1,78 @@
 "use client";
 
-import { Segment } from "@/services/boardinfo";
 import { Granboard } from "@/services/granboard";
-import Image from "next/image";
-import { useCallback, useEffect, useRef, useState } from "react";
-
+import Link from "next/link";
+import { useState } from "react";
 export default function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [granboard, setGranboard] = useState<Granboard>();
-  const [ignoreInputs, setIgnoreInputs] = useState(false);
+  const [connectionState, setConnectionState] = useState<
+    "standby" | "connecting" | "connected" | "error"
+  >("standby");
 
-  // const [notifyChar, setNotifyChar] = useState<
-  //   BluetoothRemoteGATTCharacteristic | undefined
-  // >();
-  // const connect = async () => {
-  //   const device = await navigator.bluetooth.requestDevice({
-  //     acceptAllDevices: true,
-  //     optionalServices: ["442f1570-8a00-9a28-cbe1-e1d4212d53eb"],
-  //   });
-  //   console.log(device);
-  //   const server = await device.gatt?.connect();
+  const onConnectionTest = async () => {
+    setConnectionState("connecting");
 
-  //   const service = await server?.getPrimaryService(
-  //     "442f1570-8a00-9a28-cbe1-e1d4212d53eb"
-  //   );
-  //   console.log(service);
-  //   const notifyCharacteristic = await service?.getCharacteristic(
-  //     "442f1571-8a00-9a28-cbe1-e1d4212d53eb"
-  //   );
-  //   console.log(notifyCharacteristic);
-  //   const writeCharacteristic = await service?.getCharacteristic(
-  //     "442f1572-8a00-9a28-cbe1-e1d4212d53eb"
-  //   );
-  //   console.log(writeCharacteristic);
-  //   // const service = await server.getPrimaryService(
-  //   //   "932c32bd-0000-47a2-835a-a8d455b859dd"
-  //   // );
-
-  //   notifyCharacteristic?.addEventListener(
-  //     "characteristicvaluechanged",
-  //     (ev) => {
-  //       const decoder = new TextDecoder();
-  //       const data = decoder.decode(
-  //         (ev.target as BluetoothRemoteGATTCharacteristic).value
-  //       );
-  //       console.log(data);
-  //     }
-  //   );
-  //   await notifyCharacteristic?.startNotifications();
-  //   setNotifyChar(notifyCharacteristic);
-  // };
-
-  const onSegmentHit = useCallback(
-    (segment: Segment) => {
-      // We're in a state where inputs should be ignored
-      if (ignoreInputs) {
-        return;
-      }
-
-      console.log(segment);
-    },
-    [ignoreInputs]
-  );
-
-  useEffect(() => {
-    if (!granboard) {
-      return;
+    try {
+      setGranboard(await Granboard.ConnectToBoard());
+      setConnectionState("connected");
+    } catch (error) {
+      console.error(error);
+      setConnectionState("error");
     }
-
-    granboard.segmentHitCallback = onSegmentHit;
-  }, [granboard, onSegmentHit]);
-
-  // draw darts board
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-
-    const length = 500;
-
-    console.log("loaded");
-
-    for (let index = 0; index < 20; index++) {
-      const start_rad = 0.05 * Math.PI + index * 0.1 * Math.PI;
-      const end_rad = start_rad + 0.1 * Math.PI;
-
-      // double
-      ctx.strokeStyle = "#000000";
-      ctx.fillStyle = index % 2 === 0 ? "#FF0000" : "#0000FF";
-      ctx.beginPath();
-      ctx.moveTo(250, 250); // 円の中心に筆をおろす
-      ctx.arc(250, 250, 240, start_rad, end_rad, false);
-      ctx.closePath();
-      ctx.fill();
-      ctx.stroke();
-
-      // outer single
-      ctx.strokeStyle = "#000000";
-      ctx.fillStyle = index % 2 === 0 ? "#000000" : "#FFFFFF";
-      ctx.beginPath();
-      ctx.moveTo(250, 250); // 円の中心に筆をおろす
-      ctx.arc(250, 250, 220, start_rad, end_rad, false);
-      ctx.closePath();
-      ctx.fill();
-      ctx.stroke();
-
-      // triple
-      ctx.strokeStyle = "#000000";
-      ctx.fillStyle = index % 2 === 0 ? "#FF0000" : "#0000FF";
-      ctx.beginPath();
-      ctx.moveTo(250, 250); // 円の中心に筆をおろす
-      ctx.arc(250, 250, 140, start_rad, end_rad, false);
-      ctx.closePath();
-      ctx.fill();
-      ctx.stroke();
-
-      // inner single
-      ctx.strokeStyle = "#000000";
-      ctx.fillStyle = index % 2 === 0 ? "#000000" : "#FFFFFF";
-      ctx.beginPath();
-      ctx.moveTo(250, 250); // 円の中心に筆をおろす
-      ctx.arc(250, 250, 120, start_rad, end_rad, false);
-      ctx.closePath();
-      ctx.fill();
-      ctx.stroke();
-
-      // outer bull
-      ctx.strokeStyle = "#000000";
-      ctx.fillStyle = "#FF0000";
-      ctx.beginPath();
-      ctx.arc(250, 250, 30, 0, 2 * Math.PI, false);
-      ctx.fill();
-      ctx.stroke();
-
-      // inner bull
-      ctx.strokeStyle = "#000000";
-      ctx.fillStyle = "#000000";
-      ctx.beginPath();
-      ctx.arc(250, 250, 10, 0, 2 * Math.PI, false);
-      ctx.fill();
-      ctx.stroke();
-    }
-  }, []);
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <button
-        onClick={async () => setGranboard(await Granboard.ConnectToBoard())}
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-10">
+      <Link
+        href="/01"
+        className="w-full h-20 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-4xl px-5 py-2.5 text-left"
       >
-        CONNECT
-      </button>
-      <canvas ref={canvasRef} width={500} height={500} />
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        01
+      </Link>
+      <Link
+        href="/cricket"
+        className="w-full h-20 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-4xl px-5 py-2.5 text-left"
+      >
+        Cricket
+      </Link>
+      <Link
+        href="/practice"
+        className="w-full h-20 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-4xl px-5 py-2.5 text-left"
+      >
+        Practice
+      </Link>
+      <Link
+        href="/party"
+        className="w-full h-20 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-4xl px-5 py-2.5 text-left"
+      >
+        Party
+      </Link>
+      <Link
+        href="/match"
+        className="w-full h-20 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-4xl px-5 py-2.5 text-left"
+      >
+        Match
+      </Link>
+      <Link
+        href="/ai"
+        className="w-full h-20 text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-4xl px-5 py-2.5 text-left"
+      >
+        AI
+      </Link>
+      <Link
+        href="/beta"
+        className="w-full h-20 text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-4xl px-5 py-2.5 text-left"
+      >
+        β-BETA
+      </Link>
+      <div className="grid gap-2 mb-6 md:grid-cols-2 items-center">
+        <p>connection test:</p>
+        <button
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+          onClick={onConnectionTest}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          {connectionState}
+        </button>
       </div>
     </main>
   );
